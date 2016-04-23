c7712ba78c836ba6b4e0b97665a043c5ac71a9a8
Chenglong <mestway@gmail.com>
2016-04-23 16:43:15 -0700

    aggr query added

--- src/queries.rkt
+++ src/queries.rkt
@@ -17,8 +17,13 @@
     (cons (list 1 1 2) 2)
     (cons (list 0 1 2) 2)	          
     (cons (list 1 2 1) 1)
+    (cons (list 1 2 3) 1)
     (cons (list 2 1 0) 3)))
 
+; SELECT *
+; FROM table
+; WHERE c0 < c1 
+;	AND c1 < c2 
 (define q1 (lambda (content) 
      (filter 
 	(lambda (t) 
@@ -29,6 +34,10 @@
 	content))
   )
 
+; SELECT *
+; FROM table
+; WHERE c1 < c2
+; 	AND c1 >= c0
 (define q2 
   (lambda (content)
      (filter 
@@ -40,9 +49,17 @@
        content))
   )
 
+(define (flatten-once lst)
+      (apply append lst))
+
+; subquery:
+; SELECT c2
+; FROM table
+; WHERE c0 = [0]
+; 	AND c1 = [1]
 (define q3-subquery
-  (lambda (content c0 c1) 
-    (projection 
+  (lambda (content c0 c1)
+    (projection
       (list 2)
       (filter 
       	(lambda (t)
@@ -52,22 +69,23 @@
 	      (eq? (list-ref ct 1) c1))))
       content))))
 
+; SELECT c1, c2, MAX(subquery)
 (define q3
   (lambda (content)
-    (map 
-      (lambda (p)
+    (dedup 
+      (map (lambda (p)
 	(cons 
-	  (append 
-	    (car p) 
-	    (max 
-	      (q3-subquery 
-		content 
-		(list-ref (car p) 0) 
-		(list-ref (car p) 1)))) 
-	  (cdr p)))
-      content)))
+	  (append (car p) 
+	    (list 
+	      (apply 
+		max 
+		(flatten-once 
+		  (map (lambda (t) (car t))
+		       (q3-subquery content (list-ref (car p) 0) (list-ref (car p) 1))))))) 
+	    (cdr p)))
+	content))))
 
-(print (q3 test-table))
+(println (q3 test-table))
 
-(define queries (list q1 q2))
+(define queries (list q1 q2 q3))
 
