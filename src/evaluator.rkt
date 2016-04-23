f1679f2208db98c467a5f014059d7044b1731d19
Kaiyuan Zhang <John_zh_007@yahoo.com>
2016-04-23 15:15:52 -0700

    add dedup function

---
+++ src/evaluator.rkt
@@ -0,0 +1,9 @@
+#lang rosette/safe
+
+(define (dedup table)
+  (cond [(eq? '() table) '()]
+        [else (let ([ele (car table)])
+                (cons (cons (car ele) 1)
+                      (dedup (filter (lambda (x)
+                                       (not (eq? (car ele) (car x))))
+                                     (cdr table)))))]))
