8ad1bce86ba82c0e851c9ae8db562b61c02169f2
Kaiyuan Zhang <John_zh_007@yahoo.com>
2016-04-23 15:58:20 -0700

    change eq? to equal?

--- src/evaluator.rkt
+++ src/evaluator.rkt
@@ -2,14 +2,14 @@
 
 (define (dedup table)
   (cond 
-    [(eq? '() table) '()]
+    [(equal? '() table) '()]
     [else 
       (let ([ele (car table)])
 	(cons (cons (car ele) 1)
 	      (dedup 
 		(filter 
 		  (lambda (x)
-		    (not (eq? (car ele) (car x))))
+		    (not (equal? (car ele) (car x))))
 		  (cdr table)))))]))
 
 (define (projection indices table)
