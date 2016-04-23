9a5365a31e04490c3fc2cb870eacf9e1f1fe7e94
Kaiyuan Zhang <John_zh_007@yahoo.com>
2016-04-23 15:54:08 -0700

    cross product

--- src/evaluator.rkt
+++ src/evaluator.rkt
@@ -20,3 +20,13 @@
     (map (lambda (p)
            (cons (proj-single (car p)) (cdr p)))
          table)))
+
+(define (cross-prod table1 table2)
+  (let ([cross-single (lambda (p1)
+                        (map (lambda (p2)
+                               (let ([r1 (car p1)]
+                                     [r2 (car p2)]
+                                     [cnt (* (cdr p1) (cdr p2))])
+                                 (cons (append r1 r2) cnt)))
+                             table2))])
+    (foldr append '() (map cross-single table1))))
