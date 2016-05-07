b4894668617390be1a4d47df9658b9321cb6e5b9
Shumo Chu <shumo.chu@gmail.com>
2016-05-07 16:26:45 -0700

    denote fitler

--- src/sql.rkt
+++ src/sql.rkt
@@ -41,23 +41,42 @@
 (struct val-agg (agg-func query)
 	#:transparent)
 
-;;; denote value
+;;; denote value returns tuple -> value
 (define (denote-value value nmap)
   (cond
-    [val-const? (lambda (e) (val value))]
-    [val-column-ref?
-     (lambda (e) (list-ref (hash-ref nmap (column-name value))))]
-    [val-agg?
-     (lambda (e) ((agg-func value) ((denote-sql (query value) nmap) e)))])
+    [(val-const? value) (list 'lambda '(e) (val value))]
+    [(val-column-ref? value)
+     (list 'lambda '(e) (list 'list-ref (hash-ref nmap (column-name value))))]
+    [(val-agg? value)
+     (list 'lambda '(e) (list (agg-func value) (list (denote-sql (query value) nmap) 'e)))])
      
 
 ;;; filters
 (struct filter-binop (op val1 val2))
 (struct filter-conj (f1 f2))
 (struct filter-disj (f1 f2))
-(struct filter-not (f1 f2))
+(struct filter-not (f1))
 (struct filter-exists (query))
 (struct filter-empty ())
+
+;;; denote filters returns tuple -> bool
+(define (denote-filter f nmap)
+  (cond
+    [(filter-binop? f)
+     (list 'lambda '(e) (op (list (denote-value (val1 f) nmap) 'e)
+                            (list (denote-value (val2 f) nmap) 'e)))]
+    [(filter-conj? f)
+     (list 'lambda '(e) ('and (list (denote-filter (f1 f) nmap) 'e)
+                              (list (denote-filter (f2 f) nmap) 'e)))]
+    [(filter-disj? f)
+     (list 'lambda '(e) ('or (list (denote-filter (f1 f) nmap) 'e)
+                             (list (denote-filter (f2 f) nmap) 'e)))]
+    [(filter-not? f)
+     (list 'lambda '(e) ('not (list (denote-filter (f1 f) nmap) 'e)))]
+    [(filter-exists? f)
+     (list 'lambda '(e) (list 'if (list 'empty? (list (denote-sql (query f) nmap) 'e)) '#f '#t))]
+    [(filter-empty? f) '(lambda (e) #t)]))
+     
 
 ;;; for test purpose
 
