f8b73405df5b42531ba3eb931a7d1e1170313941
Chenglong <mestway@gmail.com>
2016-05-28 14:23:03 -0700

    pullup debug

--- src/tests/aggr-pull-up.rkt
+++ src/tests/aggr-pull-up.rkt
@@ -16,9 +16,9 @@
 
 (define subq-aggr-1
   (SELECT-DISTINCT (VALS "t1.c1" (AGGR aggr-sum 
-			      (SELECT (VALS "t2.c3")
-				 FROM (AS (NAMED t1) ["t2" (list "c1" "c2" "c3")])
-				 WHERE (BINOP "t2.c1" = "t1.c1"))))
+			      (SELECT (VALS "t2.c6")
+				 FROM (AS (NAMED t1) ["t2" (list "c4" "c5" "c6")])
+				 WHERE (BINOP "t2.c4" = "t1.c1"))))
      FROM (NAMED t1)
      WHERE (filter-empty)))
 
@@ -32,6 +32,17 @@
 				 WHERE (BINOP "t2.c1" = "t1.c1"))))
     FROM (NAMED t1)
     WHERE (filter-empty)))
+			      
+(define part-ag2 (SELECT-DISTICT (VALS "t2.c1" (AGGR aggr-sum 
+			     (SELECT (VALS "t3.c3")
+				FROM (AS (NAMED t1) ["t3" (list "c1" "c2" "c3")])
+				WHERE (AND (BINOP "t3.c1" = "t2.c1") (BINOP "t3.c2" = "t2.c2")))))
+		 FROM (AS (NAMED t1) ["t2" (list "c1" "c2" "c3")])
+		WHERE (filter-empty)))
+
+(define part-ag22 (SELECT-DISTINCT (VALS "t2.c1" "t2.c2" "t2.c3")
+		 FROM (AS (NAMED t1) ["t2" (list "c1" "c2" "c3")])
+		WHERE (filter-empty)))
 
 ;(run subq-aggr-2)
 ;(run subq-aggr-1)
@@ -45,3 +56,5 @@
 (evaluate (Table-content t1) model)
 (evaluate (run subq-aggr-1) model)
 (evaluate (run subq-aggr-2) model)
+(evaluate (run part-ag2) model)
+(evaluate (run part-ag22) model)
